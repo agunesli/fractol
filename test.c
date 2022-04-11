b1da316e8ee584b593b9a06af157eae886670666
Arzu GUNESLIK <agunesli@e3r6p8.clusters.42paris.fr>
2022-04-11 11:16:44 +0200

    ght

--- test.c
+++ test.c
@@ -8,6 +8,50 @@ typedef struct	s_data {
 	int	endian;
 }	t_data;
 
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
+/*
+int	get_transparency(int trgb)
+{
+	return ((trgb >> 24) & 0xFF);
+}
+
+int	get_red(int trgb)
+{
+	return ((trgb >> 16) & 0xFF);
+}
+
+int	get_green(int trgb)
+{
+	return ((trgb >> 8) & 0xFF);
+}
+
+int	get_blue(int trgb)
+{
+	return (trgb & 0xFF);
+}
+*/
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+int	ft_close(int keycode, t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	exit(0);
+	return (0);
+}
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -18,16 +62,20 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
 	t_data	img;
-	int offset;
+	t_vars	vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "fractol");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "fractol");
+	img.img = mlx_new_image(vars.mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	offset = (y * img.line_length + x * (img.bits_per_pixel / 8));
-	mlx_loop(mlx);
+	my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
+	my_mlx_pixel_put(&img, 50, 75, 0x00FF0000);
+	my_mlx_pixel_put(&img, 75, 50, 0x00FF0000);
+	my_mlx_pixel_put(&img, 75, 75, 0x00FF0000);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_hook(vars.win, 2, 1L<<0, ft_close, &vars);
+//	mlx_hook(vars.win, 9, 1L<<5, ft_close, &vars);
+	mlx_loop(vars.mlx);
 	return (0);
 }
