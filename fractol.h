8b481d58ea9912a7514084906a6d36c8ee1350e9
agunesli <agunesli@stu.42.fr>
2022-04-14 13:30:58 +0200

    tiogr

--- fractol.h
+++ fractol.h
@@ -14,7 +14,41 @@
 # define FRACTOL_H
 # include <stdlib.h>
 # include <unistd.h>
+# include <math.h>
 # include "../mlx/mlx.h"
 
+# define WIDTH 1920
+# define HEIGHT 1080
+
+#  define ESC		65307
+
+/*#  define K_NUM_MULT	65450
+#  define K_NUM_DIV	65455
+#  define K_NUM_PLUS	65451
+#  define K_NUM_MINUS	65453
+#  define K_NUM_ENTER	65421
+#  define K_NUM_0	65438
+#  define K_UP		65362
+#  define K_DOWN	65364
+#  define K_LEFT	65361
+#  define K_RIGHT	65363
+#  define K_DIGIT_1	49
+#  define K_L		108*/
+
+typedef struct  s_vars {
+        void    *mlx;
+        void    *win;
+        int     fractal;
+	int	lock;
+	double	x_coor;
+	double	y_coor;
+	double	x_img;
+	double	y_img;
+        void    *img;
+        char    *addr;
+        int     bpp;
+        int     line_length;
+        int     endian;
+}	t_vars;
 
 #endif
--- test.c
+++ test.c
@@ -1,13 +1,5 @@
 #include "fractol.h"
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int	bits_per_pixel;
-	int	line_length;
-	int	endian;
-}	t_data;
-
 /*
 int	get_transparency(int trgb)
 {
@@ -30,12 +22,59 @@ int	get_blue(int trgb)
 }
 */
 
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
+// bpp => bits_per_pixel;
 
+int	ft_strlen(char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+void	ft_putstr(char *str)
+{
+	write(1, str, ft_stelen(str));
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	while (n && *(unsigned char *)s1
+		&& *(unsigned char *)s1 == *(unsigned char *)s2)
+	{
+		s1++;
+		s2++;
+		n--;
+	}
+	if (n)
+		return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	else
+		return (0);
+}
+
+int	found_fractal(char *fract)
+{
+	if (!ft_strncmp("Mandelbrot", fract, 11))
+		return (1);
+	else if  (!ft_strncmp("Julia", fract, 6))
+		return (2);
+	else if  (!ft_strncmp("Flocon de Koch", fract, 14))
+		return (3);
+	else
+	{
+		ft_putstr("usage : fractol [fractals]\n fractals : \n");
+		ft_putstr("--> Mandelbort\n --> Julia\n --> Flocon de Koch\n");
+		exit(EXIT_FAILURE);
+		return (0);
+	}
+}
+
+void	merror(char *str)
+{
+	ft_putstr("str");
+	exit(EXIT_FAILURE);
+}
 /*
 void	end_program(t_disp *d)
 {
@@ -46,8 +85,19 @@ void	end_program(t_disp *d)
 }
 */
 
+int	mouse_move(int x, int y, t_vars *vars)
+{
+	if (vars->fractal == 2 && vars-> lock == 0)
+	{
+		vars->x = 2 * (double)x / WIDTH - 1;
+		vars->y = 2 * (double)y / HEIGHT - 1;
+	}
+	return (0);
+}
+
 int	ft_close(int keycode, t_vars *vars)
 {
+	mlx_destroy_image(vars->mlx, vars->img);
 	mlx_destroy_window(vars->mlx, vars->win);
 	// une fonction free 
 	exit(0);
@@ -70,23 +120,38 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+// 1 => Mandelbrot 2 => Julia 3 => Flocon de Koch
 int	main(void)
 {
-	t_data	img;
 	t_vars	vars;
 
+	
+/*	if (argc != 2)
+		ft_putstr("Un seul argument est attendu\n");
+	else
+		vars.fractal = found_fractal(argv[1]);		*/
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "fractol");
-	img.img = mlx_new_image(vars.mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	if (!vars.mlx)
+		merror("Error with mlx_init\n");
+	vars.win = mlx_new_window(vars.mlx, WIDTHH, HEIGHT, "fractol");
+	if (!vars.win)
+		merror("Error with mlx_nex_window\n");
+	vars.img = mlx_new_image(vars.mlx, WIDTH, HEIGHT);
+	if (!vars.img)
+		merror("Error with mlx_new_image\n");
+	vars.addr = mlx_get_data_addr(vars.img, &vars.bpp, &vars.line_length, &vars.endian);
+	if (!vars.addr)
+		merror("Error with mlx_init\n");
+
 	my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
 	my_mlx_pixel_put(&img, 50, 75, 0x00FF0000);
 	my_mlx_pixel_put(&img, 75, 50, 0x00FF0000);
 	my_mlx_pixel_put(&img, 75, 75, 0x00FF0000);
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+
+	mlx_put_image_to_window(vars.mlx, vars.win, vars.img, 0, 0);
 	mlx_hook(vars.win, 2, 1L<<0, ft_close, &vars);
 //	mlx_hook(vars.win, 4, 1L<<2, ft_close, &vars); // press_mouse
-//	mlx_hook(vars.win, 6, 1L<<6, ft_close, &vars); // move_mouse
+//	mlx_hook(vars.win, 6, 1L<<6, mouse_move, &vars);
 	mlx_loop(vars.mlx);
 	return (0);
 }
