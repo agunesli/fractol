705e7068bb7e552b7023f1a8879137c5f903387f
Arzu GUNESLIK <agunesli@e1r7p17.42.fr>
2022-04-22 19:59:59 +0200

    rnv

---
+++ cacul.c
@@ -0,0 +1,34 @@
+#include "fractol.h"
+
+double	*mult(double z1[2], double z2[2])
+{
+	double	z[2];
+
+	z[0] = z1[0] * z2[0] - z1[1] * z2[1];
+	z[1] = z1[0] * z2[1] + z1[1] * z2[0];
+	return (z);
+}
+
+double	*dist(double z[2])
+	return (sqrt(z[0] * z[0] + z[1] * z[1]));
+
+double	geom(double c[2])
+{
+	double	z[2];
+	double	iteri;
+	double	i;
+
+	z[0] = 1;
+	z[1] = 0;
+	iteri = 10;
+	i = 0;
+	while (i < iteri)
+	{	
+		z = mult(z, c);
+		i++;
+	}	
+	if (dist(z) > xmin)
+		return (0);
+	else
+		return (1);	
+}
